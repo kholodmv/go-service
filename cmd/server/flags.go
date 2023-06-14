fa0fd5b3a7bb091b49b5d92a989f338b838623f8
Kholod Mariia Vyacheslavovna <62848971+kholodmv@users.noreply.github.com>
2023-06-14 14:55:12 +0300

    fix

--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -5,6 +5,6 @@ import "flag"
 var flagRunAddr string
 
 func parseFlags() {
-	flag.StringVar(&flagRunAddr, "a", ":8080", "address and port to run server")
+	flag.StringVar(&flagRunAddr, "a", "localhost:8080", "address and port to run server")
 	flag.Parse()
 }
