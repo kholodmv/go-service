a24ba8cd03f7d370cf1f0abd1aed595d8e068eb4
Kholod Mariia Vyacheslavovna <62848971+kholodmv@users.noreply.github.com>
2023-07-08 19:48:31 +0300

    com

--- cmd/handlers/handler.go
+++ cmd/handlers/handler.go
@@ -6,7 +6,6 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/kholodmv/go-service/cmd/storage"
 	"github.com/kholodmv/go-service/internal/gzip"
-	"github.com/kholodmv/go-service/internal/logger"
 	"github.com/kholodmv/go-service/internal/models"
 	"os"
 )
@@ -52,10 +51,10 @@ func NewHandler(router chi.Router, repository storage.MetricRepository, filename
 }
 
 func (mh *Handler) RegisterRoutes(router *chi.Mux) {
-	router.Post("/update/{type}/{name}/{value}", logger.RequestLogger(mh.UpdateMetric))
-	router.Get("/value/{type}/{name}", logger.RequestLogger(mh.GetValueMetric))
-	router.Get("/", logger.RequestLogger(mh.GetAllMetric))
+	router.Post("/update/{type}/{name}/{value}", mh.UpdateMetric)
+	router.Get("/value/{type}/{name}", mh.GetValueMetric)
+	router.Get("/", mh.GetAllMetric)
 
-	router.Post("/value/", logger.RequestLogger(mh.GetJSONMetric))
-	router.Post("/update/", logger.RequestLogger(mh.UpdateJSONMetric))
+	router.Post("/value/", mh.GetJSONMetric)
+	router.Post("/update/", mh.UpdateJSONMetric)
 }
